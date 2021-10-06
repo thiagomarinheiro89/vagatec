0b652ab1c59e8e42952992331d6efc2a61cc7624
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2021-10-05 21:52:01 -0300

    atividade boletim

--- controle/config.php
+++ controle/config.php
@@ -1,4 +1,29 @@
 <?php
+    $aluno = array();
+	$aluno['nome'] = 'Thiago';
+	$aluno['nome'] = 'Valeska';
+	$aluno['idade'] = 25;
+	$aluno['turma'] = '3TI-C';
 	
+	$aluno['materia'] = array();
+	$aluno['materia'][0] = "Programação";
+	$aluno['materia'][1] = "Banco de Dados";
+	$aluno['materia'][2] = "TCC";
 	
-?>
+	$aluno['notas'] = array();
+	$aluno['notas'][0] = 5;
+	$aluno['notas'][1] = 7;
+	$aluno['notas'][2] = 10;
+	
+	echo "<b>Nome do Aluno: </b>".$aluno['nome']."<br>";
+	echo "<b>Idade do Aluno: </b>".$aluno['idade']."<br>";
+	echo "<b>Turma do Aluno: </b>".$aluno['turma']."<br>";
+	echo "<hr>";
+	echo "<b>Boletim do aluno</b><hr>";
+	
+	for ($i = 0; $i < count($aluno['materia']); $i++)
+	{
+		echo $aluno['materia'][$i]." - Nota: <b>".$aluno['notas'][$i]."</b><br>";
+	}
+	echo "<hr>";
+	?>	
