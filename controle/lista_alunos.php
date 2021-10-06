cf60bf59a923e8fa9adbca3fa64b76bbfd17f28c
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2021-10-05 22:03:11 -0300

    atividade listar nomes do alunos

---
+++ controle/lista_alunos.php
@@ -0,0 +1,26 @@
+<?php
+	/* listar na tela todos os alunos e
+	a situação de presença ou falta
+	caso o aluno tenha faltado apresentar o
+	motivo com letras vermelha
+	caso o aluno seja o João apresentar o nome
+	do aluno em azul */
+
+	$chamada = array();
+	$chamada[0]['Nome do aluno'] = 'Thiago';
+	$chamada[0]['Presença'] = 'C';
+	$chamada[0]['motivo'] = '';
+	
+	
+	$chamada[1]['Nome do aluno'] = 'João'
+	$chamada[1]['Presença'] = 'F'
+	$chamada[1]['motivo'] = 'foi pra Liberdade';
+	
+	$chamada[2]['Nome do aluno'] = 'Vanessa'
+	$chamada[2]['Presença'] = 'F'
+	$chamada[2]['motivo'] = 'Foi fazer janta';
+
+	$chamada[3]['Nome do aluno'] = 'Valeska'
+	$chamada[3]['Presença'] = 'C'
+	$chamada[3]['motivo'] = '';
+?>
