ed07ce9eb897f39f7b1721c1c8557e2794a9ee3f
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2021-10-18 22:08:46 -0300

    Criada classe de conexao

--- controle/config.php
+++ controle/config.php
@@ -1,40 +1,17 @@
 <?php
-	/*
-		utilizando a estrutura de condicional
-		if, verifique se a nota do aluno é
-		inferior a 7 caso seja altere a cor no
-		boletim para Vermelha caso contrario
-		altere para azul
-		Ao final do boletim apresente a soma
-		das notas do aluno
-	*/
-
-
-    $aluno = array();
-	$aluno['nome'] = 'Thiago';
-	$aluno['nome'] = 'Valeska';
-	$aluno['idade'] = 25;
-	$aluno['turma'] = '3TI-C';
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$database = "vagatec";
 	
-	$aluno['materia'] = array();
-	$aluno['materia'][0] = "Programação";
-	$aluno['materia'][1] = "Banco de Dados";
-	$aluno['materia'][2] = "TCC";
+	$conexao	= mysqli_connect($host, $user, $password, $database);
 	
-	$aluno['notas'] = array();
-	$aluno['notas'][0] = 5;
-	$aluno['notas'][1] = 7;
-	$aluno['notas'][2] = 10;
+	$query = "SELECT * FROM usuarios";
 	
-	echo "<b>Nome do Aluno: </b>".$aluno['nome']."<br>";
-	echo "<b>Idade do Aluno: </b>".$aluno['idade']."<br>";
-	echo "<b>Turma do Aluno: </b>".$aluno['turma']."<br>";
-	echo "<hr>";
-	echo "<b>Boletim do aluno</b><hr>";
+	$bd = mysqli_query($conexao, $query) or die("Não foi possivel realizar a query");
 	
-	for ($i = 0; $i < count($aluno['materia']); $i++)
-	{
-		echo $aluno['materia'][$i]." - Nota: <b>".$aluno['notas'][$i]."</b><br>";
+	while($linha = mysqli_fetch_assoc($bd)){
+		echo $linha['login']." - ".$linha['senha'];
+		echo "<hr>";
 	}
-	echo "<hr>";
-	?>	
+?>
